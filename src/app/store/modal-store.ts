dde75408ca06e65df792dc6efa6a9e1464ba1ad9
inakicarcereny <inakicarce@hotmail.com.ar>
2025-03-23 18:08:58 -0300

    Implement modal store using zustand for improved modal management in the application

---
+++ src/app/store/modal-store.ts
@@ -0,0 +1,17 @@
+import { create } from 'zustand';
+
+interface ModalStore {
+  isOpen: boolean;
+  type: ModalType | null;
+  openModal: (type: ModalType) => void;
+  closeModal: () => void;
+}
+
+type ModalType = 'post';
+
+export const useModalStore = create<ModalStore>((set) => ({
+  isOpen: false,
+  type: null,
+  openModal: (type: ModalType) => set({ isOpen: true, type }),
+  closeModal: () => set({ isOpen: false, type: null }),
+}));
