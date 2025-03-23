7ae98ded224f7f8e84a8aa2c230242975b53b2a7
inakicarcereny <inakicarce@hotmail.com.ar>
2025-03-23 17:02:57 -0300

    Enhance home layout with buttons, search input, and more sections for a richer user experience on the homepage

--- src/app/(routes)/home/layout.tsx
+++ src/app/(routes)/home/layout.tsx
@@ -1,18 +1,47 @@
 import { Explorer } from '@/components/Explorer';
 import { ReactNode } from 'react';
+import { BUTTONS } from '@/lib/buttons';
+import { Search } from '@/icons/Search';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className='w-full h-screen bg-black'>
-      <div className='max-w-[1225px] w-full h-full mx-auto flex'>
+      <div className='max-w-[1225px] w-full h-full mx-auto flex gap-2'>
         <Explorer />
 
-        <main className='max-w-[600px] w-full h-full bg-yellow-100'>
+        <main className='flex flex-col max-w-[600px] w-full h-full border-x border-white/40'>
+          <header className='flex items-center justify-between w-full border border-b-white/40'>
+            {BUTTONS.map((button) => (
+              <button
+                key={button.label}
+                className='text-[#71767B] hover:bg-white/10 px-4 py-4 w-full'
+              >
+                {button.label}
+              </button>
+            ))}
+          </header>
+
           {children}
         </main>
 
-        <section className='max-w-[350px] w-full h-full bg-green-100'>
-          .
+        <section className='flex flex-col gap-8 max-w-[350px] w-full h-full text-white px-4 py-1'>
+          <div className='rounded-full border border-white/40 px-4 py-2 flex items-center gap-x-2 focus-within:border-cyan-500'>
+            <Search className='text-[#71767B]' />
+
+            <input
+              type='text'
+              className='bg-transparent w-full focus:outline-none text-sm'
+              placeholder='Search'
+            />
+          </div>
+
+          <article className='rounded-xl border border-white/40 w-full h-[350px] px-4 py-4'>
+            <h3 className='text-white text-xl'>Whats Happening</h3>
+          </article>
+
+          <article className='rounded-xl border border-white/40 w-full h-[350px] px-4 py-4'>
+            <h3 className='text-white text-xl'>Who to Follow</h3>
+          </article>
         </section>
       </div>
     </div>
--- src/app/(routes)/home/page.tsx
+++ src/app/(routes)/home/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <>
-      <div className='text-white'>home</div>
-    </>
-  );
+  return <div className='text-white'>hola</div>;
 }
