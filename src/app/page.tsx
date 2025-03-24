c32d81b823032305780e5c6c1a39f8074ae6fca9
inakicarcereny <inakicarce@hotmail.com.ar>
2025-03-24 19:52:03 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import { Button } from './components/Button';
+
 export default function Home() {
-  return <div>Hello X-clone</div>;
+  return (
+    <div>
+      <Button
+        label='Follow'
+        textSize='text-xl'
+        bgColor='bg-white'
+        textColor='text-black'
+        borderRadius='rounded-full'
+        borderSize='border'
+        borderColor='border-cyan-500'
+        paddingX='px-4'
+        paddingY='py-2'
+      />
+    </div>
+  );
 }
