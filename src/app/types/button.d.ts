f9c2b8beab1e6440cb681707586d6b69f6f7e780
inakicarcereny <inakicarce@hotmail.com.ar>
2025-03-25 20:15:19 -0300

    Update ButtonProps interface with optional properties and new types for improved customization and styling versatility

--- src/app/types/button.d.ts
+++ src/app/types/button.d.ts
@@ -5,21 +5,25 @@ export interface ButtonProps {
   textColor: TextColor;
   bgColor: BgColor;
   borderRadius: BorderRadius;
-  borderSize: BorderSize;
+  borderSize?: BorderSize;
   paddingX: PaddingX;
   paddingY: PaddingY;
   fontWeight?: FontWeight;
   borderColor?: BorderColor;
   hoverBgColor?: HoverBgColor;
+  cursor: Cursor;
+  className?: string;
+  icon?: JSX.Element;
 }
 
-type TextSize = 'text-sm' | 'text-md' | 'text-lg' | 'text-xl';
+type TextSize = 'text-sm' | 'text-md' | 'text-lg' | 'text-xl' | 'text-base';
 type TextColor = 'text-white' | 'text-black';
-type BgColor = 'bg-white' | 'bg-black';
+type BgColor = 'bg-white' | 'bg-black' | 'bg-transparent';
 type BorderRadius = 'rounded-full';
 type BorderSize = 'border';
 type PaddingX = 'px-4';
 type PaddingY = 'py-2';
 type FontWeight = 'font-semibold';
 type BorderColor = 'border-cyan-500';
-type HoverBgColor = 'hover:bg-white/10';
+type HoverBgColor = 'hover:bg-white/90';
+type Cursor = 'cursor-pointer';
