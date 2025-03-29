6f21cee2758403be2d5d16da2f8107b8266ff911
inakicarcereny <inakicarce@hotmail.com.ar>
2025-03-29 17:44:50 -0300

    Add new URL options to Urls type for better routing in layout configuration

--- src/app/utils/layout-options.ts
+++ src/app/utils/layout-options.ts
@@ -1,4 +1,9 @@
-type Urls = '/home/connect-people' | '/home/profile';
+type Urls =
+  | '/home/connect-people'
+  | '/home/profile'
+  | 'home/emilys'
+  | 'home/michaelw'
+  | 'home/sophiab';
 
 export function isHeaderHidden(pathname: string, urls: Urls[]) {
   return urls.some((url) => pathname.includes(url));
